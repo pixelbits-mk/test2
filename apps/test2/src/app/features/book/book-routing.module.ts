4e797a01c135320bead98ee6836ce02a7ff18ceb
Michael Kang <kang.mike@gmail.com>
2021-08-23 04:54:32 -0400

    updated

--- apps/test2/src/app/features/book/book-routing.module.ts
+++ apps/test2/src/app/features/book/book-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { AngularFireAuthGuard, AuthPipe, customClaims, hasCustomClaim, loggedIn, redirectUnauthorizedTo } from "@angular/fire/auth-guard";
 import { RouterModule, Routes } from "@angular/router";
-import { forkJoin, iif, of, pipe } from "rxjs";
+import { forkJoin, iif, Observable, of, pipe, UnaryFunction, zip } from "rxjs";
 import { map, switchMap, tap } from "rxjs/operators";
 import { BookComponent } from "./book.component";
 import firebase from 'firebase/app'
@@ -35,48 +35,112 @@ import firebase from 'firebase/app'
 //     //     return ['unauthorized']
 //     // }))
 // })
+// const z: Observable<firebase.User> = of(null)
+// const loggedin2 = loggedIn(z)
+// const customClaims2 = customClaims(z)
+// const hasCustomClaim2 = hasCustomClaim('admin')(z)
+// const hasCustomClaim3 = hasCustomClaim('moderator')(z)
+
+const combineAuthPipes = (authPipes: (AuthPipe |  UnaryFunction<Observable<firebase.User>, Observable<unknown>>)[]) => 
+    switchMap((t: Observable<firebase.User>) => forkJoin(authPipes.map(x => x(t))))
 
 const loggedInThenRedirect = pipe(
-    switchMap((t: firebase.User) => {
-        return forkJoin([
-            loggedIn(of(t)),
-            customClaims(of(t)),
-            hasCustomClaim('admin')(of(t)),
-            hasCustomClaim('moderator')(of(t)),
-        ]).pipe(
-            map(([isLoggedIn, customClaimList, admin, moderator]) => {
-                return {
-                    loggedIn: isLoggedIn,
-                    customClaims: customClaimList,
-                    admin, 
-                    moderator
-                }
-            }),
-            map((t) => {
-                console.log(t)
-                if (t.admin) {
-                    return true
-                }
-                if (!t.loggedIn) {
-                    return ['login']
-                }
-                return ['unauthorized']
-            })
-
-        )
-        // customClaims(of(t)).subscribe(y => {
-        //     console.log('claims', y)
-        // })
-        // if (!t) {
-        //     return of(['login'])
-        // }
-        // return hasCustomClaim('admin')(of(t)).pipe(
-        //     map(x => !x ?  ['unauthorized'] : true)
-        // )
-
+    map((t: firebase.User) => of(t)),
+    combineAuthPipes([
+        loggedIn,
+        customClaims,
+        hasCustomClaim('admin'),
+        hasCustomClaim('moderator')
+    ]),
+    map(([isLoggedIn, customClaimList, admin, moderator]) => {
+        return {
+            loggedIn: isLoggedIn,
+            customClaims: customClaimList,
+            admin, 
+            moderator
+        }
     }),
-    tap(t => console.log(t))
+    map((t) => {
+        console.log(t)
+        if (t.admin) {
+            return true
+        }
+        if (!t.loggedIn) {
+            return ['login']
+        }
+        return ['unauthorized']
+    })
 )
+// const loggedInThenRedirect = pipe(
+//     map((t: firebase.User) => of(t)),
+//     switchMap((t: Observable<firebase.User>) => {
+//         return forkJoin([
+//             loggedIn(t),
+//             customClaims(t),
+//             hasCustomClaim('admin')(t),
+//             hasCustomClaim('moderator')(t)
+//         ])
+//     }),
+//     map(([isLoggedIn, customClaimList, admin, moderator]) => {
+//         return {
+//             loggedIn: isLoggedIn,
+//             customClaims: customClaimList,
+//             admin, 
+//             moderator
+//         }
+//     }),
+//     map((t) => {
+//         console.log(t)
+//         if (t.admin) {
+//             return true
+//         }
+//         if (!t.loggedIn) {
+//             return ['login']
+//         }
+//         return ['unauthorized']
+//     })
+// )
+// const loggedInThenRedirect = pipe(
+//     switchMap((t: firebase.User) => {
+//         return forkJoin([
+//             loggedIn(of(t)),
+//             customClaims(of(t)),
+//             hasCustomClaim('admin')(of(t)),
+//             hasCustomClaim('moderator')(of(t)),
+//         ]).pipe(
+//             map(([isLoggedIn, customClaimList, admin, moderator]) => {
+//                 return {
+//                     loggedIn: isLoggedIn,
+//                     customClaims: customClaimList,
+//                     admin, 
+//                     moderator
+//                 }
+//             }),
+//             map((t) => {
+//                 console.log(t)
+//                 if (t.admin) {
+//                     return true
+//                 }
+//                 if (!t.loggedIn) {
+//                     return ['login']
+//                 }
+//                 return ['unauthorized']
+//             })
+
+//         )
+//         // customClaims(of(t)).subscribe(y => {
+//         //     console.log('claims', y)
+//         // })
+//         // if (!t) {
+//         //     return of(['login'])
+//         // }
+//         // return hasCustomClaim('admin')(of(t)).pipe(
+//         //     map(x => !x ?  ['unauthorized'] : true)
+//         // )
+
+//     }),
+//     tap(t => console.log(t))
+// )
 
 
 
